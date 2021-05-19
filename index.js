2eff07bdcb938e114c792bbf6e80f815a94f852e
YevaUsatova <yeva.usatova@gmail.com>
2021-05-18 21:57:05 -0400

    commit tree d383fd6866acb19cc3e85a31ab8680af240f7cc9

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(fun='roller-skate'){
+    return `This Saturday, I want to ${fun}!`;
+}
+const mondayWork = function (activ="go to the office"){
+    return `This Monday, I will ${activ}.`;
+}
+mondayWork();
+function wrapAdjective(val="*"){
+     return function (par="special"){
+        return `You are ${val}${par}${val}!`;
+    }
+
+}
+wrapAdjective("%")("a dedicated programmer");
